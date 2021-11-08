af2d2ecd26531d562c3eab2e9bc9fbf0aba7b6dd
Balazshazialex <balazshazia@gmail.com>
2021-11-08 23:33:31 +0200

    Added login frontend

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from "./pages/Login";
 
 const App: React.FC = () => (
   <IonApp>
@@ -29,8 +30,11 @@ const App: React.FC = () => (
         <Route exact path="/home">
           <Home />
         </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/login" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
