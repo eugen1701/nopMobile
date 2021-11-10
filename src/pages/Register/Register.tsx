fffffc3a071ef3b1c6fc8c2a7eb9c384c5c34138
cborodi <cristi_borodi@yahoo.com>
2021-11-10 10:57:35 +0200

    register fronted added

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,159 @@
+import {
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonCheckbox,
+    IonButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonAlert,
+    IonIcon,
+    IonPage,
+    IonImg
+
+} from '@ionic/react';
+import {useHistory} from "react-router";
+import React, {useState} from "react";
+
+import './Register.css';
+
+import {pizza} from "ionicons/icons";
+
+const Register: React.FC = () => {
+    const history = useHistory();
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordConfirmed, setPasswordConfirmed] = useState<string>("");
+    const [iserror, setIserror] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>("");
+
+    const handleRegister = () => {
+        if (password != passwordConfirmed) {
+            setMessage("The two password fields must match!");
+            setIserror(true);
+            return;
+        }
+
+        // Check if the name/email is not already used (or just email)
+        // Email format error/name/password checked in backend?
+
+        const loginData = {
+            "name": name,
+            "email": email,
+            "password": password
+        }
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Sign up</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen className="ion-padding ion-text-center">
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonIcon
+                                style={{ fontSize: "40px", color:"#DEA54B" }}
+                                icon={pizza}
+                            />
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel class="header-text" position="floating">
+                                Sign up to see all the available kitchens in your city
+                                <br/><br/>
+                            </IonLabel>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Name</IonLabel>
+                                <IonInput
+                                    type="text"
+                                    value={name}
+                                    onIonChange={(e) => setName(e.detail.value!)}
+                                >
+                                </IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Email</IonLabel>
+                                <IonInput
+                                    type="email"
+                                    value={email}
+                                    onIonChange={(e) => setEmail(e.detail.value!)}
+                                >
+                                </IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Password</IonLabel>
+                                <IonInput
+                                    type="password"
+                                    value={password}
+                                    onIonChange={(e) => setPassword(e.detail.value!)}
+                                >
+                                </IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Confirm Password</IonLabel>
+                                <IonInput
+                                    type="password"
+                                    value={passwordConfirmed}
+                                    onIonChange={(e) => setPasswordConfirmed(e.detail.value!)}
+                                >
+                                </IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonButton expand="block"  onClick={handleRegister}>Sign up</IonButton>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow>
+                        <IonCol>
+                            <IonAlert
+                                isOpen={iserror}
+                                onDidDismiss={() => setIserror(false)}
+                                header={"Error!"}
+                                message={message}
+                                buttons={["Dismiss"]}
+                            />
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Register;
